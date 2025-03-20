ec6e9e3727fce533500e0d3e6f1481ba95ad7d68
Dmitry <er_92@list.ru>
2025-03-20 22:39:59 +0300

    add SmashWords kata

---
+++ src/main/scala/playgraund/Katas/SmashWords.scala
@@ -0,0 +1,18 @@
+package playgraund.Katas
+
+@main
+def main(): Unit =
+
+  val r = smash_mkString(List("aa"))
+
+  println(s"smash =$r")
+
+def smash_mkString(words: List[String]): String =
+  words.mkString(" ")
+
+
+def smash_reduce(words: List[String]): String =
+  if (words.nonEmpty) words.reduce(_ + " " + _) else ""
+
+def smash_reduceOption(words: List[String]): String =
+  words.reduceOption(_ + " "+ _).getOrElse("")
---
+++ src/test/scala/Katas/SmashWordsTest.scala
@@ -0,0 +1,27 @@
+package Katas
+
+import org.scalatest.funsuite.AnyFunSuite
+import playgraund.Katas.{smash_mkString, smash_reduce, smash_reduceOption}
+
+class SmashWordsTest extends AnyFunSuite:
+  test("one string as is") {
+
+    assert(
+      List(smash_mkString, smash_reduce, smash_reduceOption).map(f =>
+        f(List("aa"))
+      ) === List.fill(3)("aa")
+    )
+
+  }
+
+  test("several strings as sentence") {
+
+    assert(smash_mkString(List("aa", "bb")) === "aa bb")
+
+  }
+
+  test("empty list as empty string") {
+
+    assert(smash_mkString(List()) === "")
+
+  }
