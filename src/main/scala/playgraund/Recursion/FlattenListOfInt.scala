5212e6f6f0249df39e261905c171c14aeefb4837
Dmitry <er_92@list.ru>
2025-04-22 14:22:14 +0300

    add flatten

---
+++ src/main/scala/playgraund/Recursion/FlattenListOfInt.scala
@@ -0,0 +1,13 @@
+package playgraund.Recursion
+
+object FlattenListOfInt extends App{
+  println(flatten(List(1,2,List(3,4,List(5,6)))))
+}
+
+def flatten(l: List[Any]): List[Any] =
+  l match
+    case Nil => Nil
+    case h :: t =>
+      h match
+        case x: Int => h :: flatten(t)
+        case x: List[Any] => flatten(x) ::: flatten(t)
